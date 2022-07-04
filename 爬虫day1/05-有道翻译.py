7049c6ddc1298d4555fbeec1305006d47795e07a
Michael <a916250727@163.com>
2022-07-04 23:37:04 +0800

    woniule

---
+++ 爬虫day1/05-有道翻译.py
@@ -0,0 +1,26 @@
+# 作者: Michael(phb)
+# 2022年07月04日23时18分50秒
+import requests
+
+datas = {
+    "type": "AUTO",
+    "i": "i love python",
+    "doctype": "json",
+    "xmlVersion": "1.8",
+    "keyfrom": "fanyi.web",
+    "ue": "UTF-8",
+    "action": "FY_BY_ENTER",
+    "typoResult": "true"
+}
+
+url = "http://fanyi.youdao.com/translate?smartresult=dict&smartresult=rule&smartresult=ugc&sessionFrom=null"
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"}
+
+response = requests.post(url, data=datas, headers=headers)
+
+print(response.text)
+
+# 如果是json文件可以直接显示
+print(response.json())
