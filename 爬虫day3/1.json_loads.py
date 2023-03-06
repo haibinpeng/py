be4e04cb9f033470c81d5d0e80958ad2d634515c
Michael <a916250727@163.com>
2023-03-06 23:46:03 +0800

    woniule

---
+++ 爬虫day3/1.json_loads.py
@@ -0,0 +1,39 @@
+# 作者: Michael(phb)
+# 2023年03月06日22时34分12秒
+
+# 理解json.loads，json.loads()把json字符串转化为python类型
+
+# import json
+#
+# strList = '[1, 2, 3, 4]'
+# strDict = '{"city": "北京", "name": "大猫"}'
+# print(type(strDict))
+#
+# print(json.loads(strList))
+# print(type(json.loads(strList)))
+#
+# print(json.loads(strDict))
+# print(type(json.loads(strDict)))
+
+
+# 把豆瓣电影的json对象转换为dict类型
+
+import json
+import parse_url
+from pprint import pprint
+
+url = "https://movie.douban.com/j/search_subjects?type=movie&tag=%E7%83%AD%E9%97%A8&page_limit=50&page_start=0"
+html_str = parse_url.parse_url(url)
+print(type(html_str))
+
+ret1 = json.loads(html_str)
+pprint(ret1)
+print(type(ret1))
+
+# with open('douban1.json', 'w', encoding='utf-8') as f:
+#     # json.dump()将python内置类型序列转化为json对象后写入文件
+#     json.dump(ret1, f, ensure_ascii=False, indent=2)
+
+# 如果使用json.dumps()，需要write
+with open('douban2.json', 'w', encoding='utf-8') as f:
+    f.write(json.dumps(ret1, ensure_ascii=False, indent=2))
---
+++ 爬虫day3/parse_url.py
@@ -0,0 +1,35 @@
+# 作者: Michael(phb)
+# 2023年03月06日22时47分58秒
+
+import requests
+from retrying import retry
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36"
+}
+
+
+# 使用retry尝试多次请求
+@retry(stop_max_attempt_number=3)  # 装饰器，最多尝试3次
+def _parse_url(url, method, data, proxies):
+    print("*" * 20)
+    if method == "POST":
+        response = requests.post(url, data=data, headers=headers, proxies=proxies)
+    else:
+        response = requests.get(url, headers=headers, timeout=3, proxies=proxies)
+    assert response.status_code == 200  # 断言，如果！=200，程序就会直接在这里崩掉，不会再执行下面的return
+    return response.content.decode()
+
+
+def parse_url(url, method="GET", data=None, proxies={}):  # proxies可以使用代理
+    try:
+        html_str = _parse_url(url, method, data, proxies)
+    except:
+        html_str = None
+    return html_str
+
+
+if __name__ == '__main__':
+    url = "https://movie.douban.com/j/search_subjects?type=movie&tag=%E7%83%AD%E9%97%A8&page_limit=50&page_start=0"
+    print(parse_url(url))
+    print(type(parse_url(url)))
